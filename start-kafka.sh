a6ada7a8d23b2e4ec627d62ab1ef37f0c1396361
Orange Mi <orangemiwj@gmail.com>
2016-10-13 14:16:28 +0800

    optimize

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "$KAFKA_PORT" ]]; then
-    export KAFKA_PORT=9092
+  export KAFKA_PORT=9092
 fi
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
   # By default auto allocate broker ID
@@ -29,9 +29,9 @@ do
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+      sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
     else
-        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+      echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
     fi
   fi
 done
@@ -39,7 +39,6 @@ done
 if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
   eval $CUSTOM_INIT_SCRIPT
 fi
-
 
 KAFKA_PID=0
 
@@ -53,7 +52,6 @@ term_handler() {
   echo 'Kafka stopped.'
   exit
 }
-
 
 # Capture kill requests to stop properly
 trap "term_handler" SIGHUP SIGINT SIGTERM
